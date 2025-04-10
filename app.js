ce230053d5390b521ad9238a4b2f1270646cc2ed
Prakhar Sharma <p7922755@gmail.com>
2025-04-10 05:38:07 +0530

    second commit

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-let todo=[]
-let task=prompt("say somthing")
-while(true)
-{
-    
-    if(task==="quit"){
+let todo = []
+let task = prompt("say somthing")
+while (true) {
+
+    if (task === "quit") {
         break
     }
-    if(task==="new")
-    {
-        let newTask=prompt("Add a new task")
+    if (task === "new") {
+        let newTask = prompt("Add a new task")
         todo.push(newTask)
     }
-    else if(task==="delete")
-    {
-        let index=parseInt(prompt("Give the index of the task you want to delete"))
-        if(!Number.isNaN(index)){
-            todo.splice(index,1)
+    else if (task === "delete") {
+        let index = parseInt(prompt("Give the index of the task you want to delete"))
+        if (!Number.isNaN(index)) {
+            todo.splice(index, 1)
         }
-        else{
+        else {
             console.log("unknown number")
         }
     }
-    else if(task==="list")
-    {
+    else if (task === "list") {
         console.log("***************")
-        for(let i of todo){
+        for (let i of todo) {
             console.log(`${todo.indexOf(i)}:${i}`)
         }
         console.log("***************")
     }
-    task=prompt("say")
-    task=task.trim()
+    task = prompt("say")
+    task = task.trim()
 }
+console.log("dfgdgdgdgdg")
